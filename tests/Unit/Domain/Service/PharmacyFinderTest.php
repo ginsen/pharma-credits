07c8083e3df7ba40ba4fa2419fec90c98b08cca9
J. Gines Hernandez Galindo <jgines@gmail.com>
2019-09-01 22:56:11 +0200

    test domain service

---
+++ tests/Unit/Domain/Service/PharmacyFinderTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Domain\Service;
+
+use App\Domain\Common\Specification\SpecificationInterface;
+use App\Domain\Entity\Pharmacy;
+use App\Domain\Exception\PharmacyException;
+use App\Domain\Repository\PharmacyReadModelInterface;
+use App\Domain\Service\PharmacyFinder;
+use App\Domain\Specification\PharmacySpecificationFactoryInterface;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+use Mockery as m;
+
+class PharmacyFinderTest extends TestCase implements PharmacyReadModelInterface, PharmacySpecificationFactoryInterface
+{
+    protected $pharmacy;
+
+
+    /**
+     * @test
+     * @throws \Exception
+     */
+    public function it_should_return_exception_when_not_found_pharmacy()
+    {
+        $this->expectException(PharmacyException::class);
+        $this->pharmacy = null;
+
+        $finder = new PharmacyFinder($this, $this);
+        $finder->findOneOrFailByUuid(Uuid::uuid4());
+    }
+
+
+    /**
+     * @test
+     * @throws \Exception
+     */
+    public function it_should_return_one_pharmacy()
+    {
+        $this->pharmacy = m::mock(Pharmacy::class);
+
+        $uuid = Uuid::uuid4();
+        $finder = new PharmacyFinder($this, $this);
+
+        self::assertSame($this->pharmacy, $finder->findOneOrFailByUuid($uuid));
+    }
+
+
+
+    public function getOneOrNull(SpecificationInterface $specification): ?Pharmacy
+    {
+        return $this->pharmacy;
+    }
+
+
+    public function createForFindOneWithUuid(UuidInterface $uuid): SpecificationInterface
+    {
+        return m::mock(SpecificationInterface::class);
+    }
+}
