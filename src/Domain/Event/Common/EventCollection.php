9627bf339f9727a838074d9a7a44a84080c26578
J. Ginés Hernández G <jgines@yaencontre.com>
2019-09-19 16:10:03 +0200

    fixed typo

--- src/Domain/Event/Common/EventCollection.php
+++ src/Domain/Event/Common/EventCollection.php
@@ -8,7 +8,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 
 class EventCollection
 {
-    /** @var self|null  */
+    /** @var self|null */
     private static $instance = null;
 
     /** @var ArrayCollection */
