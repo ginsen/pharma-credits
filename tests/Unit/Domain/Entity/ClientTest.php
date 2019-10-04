1792eb9437aadc7c4d8baf38c06d6f18939e6b9c
J. Gines Hernandez G <jgines@gmail.com>
2019-10-04 12:59:29 +0200

    add Test

--- tests/Unit/Domain/Entity/ClientTest.php
+++ tests/Unit/Domain/Entity/ClientTest.php
@@ -8,6 +8,7 @@ use App\Domain\Entity\Client;
 use App\Domain\Entity\Pharmacy;
 use App\Domain\Entity\Point;
 use App\Domain\Exception\ClientException;
+use App\Domain\Exception\PointException;
 use App\Domain\ValueObj\AwardedAt;
 use App\Domain\ValueObj\ClientName;
 use App\Domain\ValueObj\PharmacyName;
@@ -23,7 +24,7 @@ class ClientTest extends TestCase
      * @test
      * @throws AssertionFailedException|\Exception
      */
-    public function it_should_create_one_instance()
+    public function it_should_create_one_instance(): void
     {
         $uuid   = Uuid::uuid4();
         $name   = ClientName::fromStr('test client');
@@ -37,7 +38,7 @@ class ClientTest extends TestCase
      * @test
      * @throws AssertionFailedException|\Exception
      */
-    public function it_should_return_name_and_uuid()
+    public function it_should_return_name_and_uuid(): void
     {
         $uuid   = Uuid::uuid4();
         $name   = ClientName::fromStr('test client');
@@ -52,7 +53,7 @@ class ClientTest extends TestCase
      * @test
      * @throws AssertionFailedException|\Exception
      */
-    public function it_should_return_only_available_points()
+    public function it_should_return_only_available_points(): void
     {
         $uuid   = Uuid::uuid4();
         $name   = ClientName::fromStr('test client');
@@ -73,7 +74,7 @@ class ClientTest extends TestCase
      * @test
      * @throws AssertionFailedException|\Exception
      */
-    public function it_should_return_partial_quantity_of_available_points()
+    public function it_should_return_partial_quantity_of_available_points(): void
     {
         $uuid   = Uuid::uuid4();
         $name   = ClientName::fromStr('test client');
@@ -89,7 +90,7 @@ class ClientTest extends TestCase
      * @test
      * @throws AssertionFailedException|\Exception
      */
-    public function it_should_launch_exception_when_require_invalid_quantity_points()
+    public function it_should_launch_exception_when_require_invalid_quantity_points(): void
     {
         $this->expectException(ClientException::class);
 
@@ -99,6 +100,23 @@ class ClientTest extends TestCase
 
         $this->addPoints($client, 2);
         $client->getPoints(QuantityPoints::fromInt(3));
+    }
+
+
+    /**
+     * @test
+     * @throws AssertionFailedException|\Exception
+     */
+    public function it_should_launch_exception_when_add_invalid_point(): void
+    {
+        $this->expectException(PointException::class);
+
+        $uuid   = Uuid::uuid4();
+        $name   = ClientName::fromStr('test client');
+        $client = Client::create($uuid, $name);
+
+        $point = $this->createBadPoint();
+        $client->addPoint($point);
     }
 
 
@@ -117,5 +135,25 @@ class ClientTest extends TestCase
             --$number;
             Point::createAwardPoint($client, $pharmacy, $time);
         }
+    }
+
+
+    /**
+     * @return Point
+     */
+    protected function createBadPoint(): Point
+    {
+        return new class extends Point {
+
+            public function __construct()
+            {
+                parent::__construct();
+            }
+
+            public function isAvailableForClient(): bool
+            {
+                return false;
+            }
+        };
     }
 }
